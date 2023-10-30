0d8b98b7f9f151199bb53224b9df3fa2d8c11470
Chen, Zhentao <zhentao.chen@intel.com>
2023-10-30 12:18:01 +0800

    fix if error

--- prepare-bigdl-llb-env.sh
+++ prepare-bigdl-llb-env.sh
@@ -5,10 +5,11 @@
 conda create -n llb_test python=3.9 -y
 conda activate llb_test
 
-pip install --pre --upgrade bigdl-nano[pytorch]
+pip install --pre --upgrade bigdl-nano
 source bigdl-nano-init
 
-if [$1 -eq "xpu"] then
+if [ "$1" == "xpu" ]
+then
     pip install --pre --upgrade bigdl-llm[xpu] -f https://developer.intel.com/ipex-whl-stable-xpu
     echo 'source /opt/intel/oneapi/setvars.sh' >> $CONDA_PREFIX/etc/conda/activate.d
 
@@ -19,8 +20,6 @@ else
     pip install --pre --upgrade bigdl-llm[all]
 
 pip install -e .
-pip install --pre --upgrade bigdl-nano
-source bigdl-nano-init
 
 conda env config vars set HF_HOME=$HF_HOME
 conda env config vars set HF_DATASETS_CACHE=$HF_HOME/datasets
